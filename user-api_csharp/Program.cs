50dc3051496c598bfb6cfa1c31963d48c2c4fad6
David Calderón <dcalderonlo@outlook.com>
2026-02-27 21:31:59 -0400

    fix: Update namespaces and enable browser launch for development in launch settings

--- user-api_csharp/Program.cs
+++ user-api_csharp/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using user_api_csharp.Data;
+using user_api_csharp.src.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,18 +8,30 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+  var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+  dbContext.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.MapGet("/", () => Results.Ok(new
+{
+  message = "User API is running.",
+  swagger = "/swagger",
+  users = "/api/users"
+}));
 
 app.Run();
